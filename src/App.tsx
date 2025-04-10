8d7bbb7e339b6ecabe6017a376f2b5eaa8eb1a19
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-11 03:48:45 +0400

    add footer Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      {" "}
+      <Routes />
     </>
   );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,23 +1,40 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Outlet,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
 import NotFound from "./pages/NotFound";
+import Footer from "./pages/Footer"; // Import Footer
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: (
+      <div className='min-h-screen flex flex-col'>
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/location",
+        element: <Location />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
-  {
-    path:"/location",
-    element: <Location />
-  },
-  {
-    path:"*",
-    element: <NotFound />
-  }
 ]);
-const  Routes=()=> {
-    return <RouterProvider router={router} />;
-}
+
+const Routes = () => {
+  return <RouterProvider router={router} />;
+};
+
 export default Routes;
